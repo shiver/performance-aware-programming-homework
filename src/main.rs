d13e93d115e5bb556bd6d7ec80c21bef41e537ff
Robert Vally <rvally@gmail.com>
2023-03-10 14:45:00 +0800

    Passing listing 39

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use byteorder::ReadBytesExt;
+use byteorder::{LittleEndian, ReadBytesExt};
 use std::{
     fs::File,
     io::{Cursor, Read},
-    path::Path,
 };
 
 fn load_binary(path: &str) -> Result<Vec<u8>> {
@@ -16,6 +15,7 @@ fn load_binary(path: &str) -> Result<Vec<u8>> {
 }
 
 #[derive(Debug, PartialEq)]
+#[allow(dead_code)]
 enum ModField {
     MemoryNoDisplacement = 0b00,
     MemoryDisplacement8bit = 0b01,
@@ -25,6 +25,7 @@ enum ModField {
 
 #[repr(u8)]
 #[derive(Debug)]
+#[allow(non_camel_case_types, dead_code)]
 enum Registers {
     al = 0b0000,
     cl = 0b0001,
@@ -44,6 +45,8 @@ enum Registers {
     di = 0b1111,
 }
 
+const SHOW_COMMENTS: bool = false;
+
 fn main() -> Result<()> {
     let binary_filename = std::env::args()
         .nth(1)
@@ -54,12 +57,12 @@ fn main() -> Result<()> {
 
     println!("bits 16");
 
-    loop {
+    'main: loop {
         println!("");
 
         let opcode = match cursor.read_u8() {
             Ok(byte) => byte,
-            Err(_) => break
+            Err(_) => break,
         };
 
         if (opcode >> 2) == 0b100010 {
@@ -71,29 +74,111 @@ fn main() -> Result<()> {
             let next_byte = cursor.read_u8()?;
 
             let rm = next_byte & 0b111;
-            let mod_field = unsafe { std::mem::transmute::<u8, ModField>(next_byte >> 6) };
+            let mode = unsafe { std::mem::transmute::<u8, ModField>(next_byte >> 6) };
             let reg = unsafe {
                 std::mem::transmute::<u8, Registers>(w_field << 3 | (next_byte >> 3 & 0b111))
             };
 
             print!("mov ");
 
-            if mod_field == ModField::RegisterNoDisplacement {
-                let other_reg = unsafe { std::mem::transmute::<u8, Registers>(w_field << 3 | rm) };
-
-                match d_field {
-                    0 => print!("{:?}, {:?}", other_reg, reg),
-                    1 => print!("{:?}, {:?}", reg, other_reg),
-                    _ => (),
+            let value: i16 = match mode {
+                ModField::MemoryDisplacement8bit => cursor.read_i8()? as i16,
+                ModField::MemoryDisplacement16bit => cursor.read_i16::<LittleEndian>()?,
+                ModField::MemoryNoDisplacement if rm == 0b110 => {
+                    cursor.read_i16::<LittleEndian>()?
                 }
+                _ => 0,
+            };
+
+            let (source, destination) = match mode {
+                ModField::MemoryNoDisplacement => {
+                    let memory = match rm {
+                        0b000 => format!("[bx + si]"),
+                        0b001 => format!("[bx + di]"),
+                        0b010 => format!("[bp + si]"),
+                        0b011 => format!("[bp + di]"),
+                        0b100 => format!("[si]"),
+                        0b101 => format!("[di]"),
+                        0b110 if value == 0 => format!("[bp]"),
+                        0b110 => format!("[{}]", value),
+                        0b111 => format!("[bx]"),
+
+                        _ => unreachable!(),
+                    };
+
+                    match d_field {
+                        0 => (memory, format!("{:?}", reg)),
+                        1 => (format!("{:?}", reg), memory),
+                        _ => unreachable!(),
+                    }
+                }
+
+                ModField::MemoryDisplacement16bit | ModField::MemoryDisplacement8bit => {
+                    let memory = match rm {
+                        0b000 => format!("[bx + si + {}]", value),
+                        0b001 => format!("[bx + di + {}]", value),
+                        0b010 => format!("[bp + si + {}]", value),
+                        0b011 => format!("[bp + di + {}]", value),
+                        0b100 => format!("[si + {}]", value),
+                        0b101 => format!("[di + {}]", value),
+                        0b110 if value == 0 => format!("[bp]"),
+                        0b110 => format!("[{}]", value),
+                        0b111 => format!("[bx + {}]", value),
+
+                        _ => unreachable!(),
+                    };
+
+                    match d_field {
+                        0 => (memory, format!("{:?}", reg)),
+                        1 => (format!("{:?}", reg), memory),
+                        _ => unreachable!(),
+                    }
+                }
+
+                ModField::RegisterNoDisplacement => {
+                    let other_reg =
+                        unsafe { std::mem::transmute::<u8, Registers>(w_field << 3 | rm) };
+
+                    match d_field {
+                        0 => (format!("{:?}", other_reg), format!("{:?}", reg)),
+                        1 => (format!("{:?}", reg), format!("{:?}", other_reg)),
+                        _ => unreachable!(),
+                    }
+                }
+            };
+
+            print!("{}, {}", source, destination);
+
+            if SHOW_COMMENTS {
+                print!(
+                    " ; Register/memory to/from register \n\topcode={:#b} next_byte={:#b}\n\td={} w={} mod={:#03b} reg={:?} rm={:#b}\n",
+                    opcode, next_byte, d_field, w_field, mode as u8, reg, rm
+                );
             }
         } else if opcode >> 1 == 0b1100011 {
             // MOV - Immediate to register/memory
             println!("mov ;Immediate to register/memory");
+
+            // let w_field = opcode & 0b1;
+
+            let next_byte = cursor.read_u8()?;
+            let mode = unsafe { std::mem::transmute::<u8, ModField>(next_byte >> 6) };
+
+            if mode == ModField::RegisterNoDisplacement {}
+
+            break 'main;
         } else if opcode >> 4 == 0b1011 {
-            // MOV - Register/memory to/from register
-            println!("mov ;Register/memory to/from register");
-            // cursor.read_u24();
+            // MOV - Immediate to register
+            let w_field = opcode >> 3 & 0b1;
+            let reg =
+                unsafe { std::mem::transmute::<u8, Registers>(w_field << 3 | (opcode & 0b111)) };
+
+            print!("mov {:?}, ", reg);
+            if w_field == 0 {
+                print!("{}", cursor.read_i8()?);
+            } else {
+                print!("{}", cursor.read_i16::<LittleEndian>()?);
+            }
         } else if opcode >> 1 == 0b1010000 {
             // MOV - Memory to accumulator
             println!("mov ;Memory to accumulator");
@@ -107,7 +192,6 @@ fn main() -> Result<()> {
             // MOV - Segment register to register/memory
             println!("mov ;Segment register to register/memory");
         }
-
     }
 
     Ok(())
